40fbb08cbfe5ca9812e01c88c811c4acb68930fa
ZCrAzYF <ivan.235207@nv.ccsd.net>
2022-04-08 08:28:45 -0700

    Update workspace.js

--- workspace.js
+++ workspace.js
@@ -1,4 +1,4 @@
-const TaskList = [
+var TaskList = [
     {Task: 'Finish AP CSP code project', date: new Date('2022-04-08'
     )}, {task: 'finish late work', date: new Date('2022-04-29'
     )}, {task: 'learn more programing', date: new Date('2022-04-07'
@@ -16,6 +16,6 @@ TaskList.push({Task: "add a item to list", date: new Date("2022-04-15")});
 
 const sortedTasks = TaskList.slice().sort((a,b) => b.date - a.date);
 
-document.getElementById("printlist").innerHTML = JSON.stringify(TaskList,null,4);
+document.getElementById("printlist").innerHTML = JSON.stringify(TaskList, null, 4);
 
 console.log(sortedTasks);
