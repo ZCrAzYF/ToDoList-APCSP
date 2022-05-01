29e3cc02e4ce05a6121c329b0e3c911f356c8ffc
ZCrAzYF <ivan.235207@nv.ccsd.net>
2022-05-01 08:21:19 -0700

    Update workspace.js

--- workspace.js
+++ workspace.js
@@ -1,8 +1,8 @@
 //Sets up array(list) as an object and sets peramiters / pre-fills list with 3 tasks
 var TaskList = [
-    {Task: 'Finish AP CSP code project', date: new Date('2022/04/08'
-    )}, {task: 'finish late work', date: new Date('2022/04/29'
-    )}, {task: 'learn more programing', date: new Date('2022/04/07'
+    {Task: 'Finish AP CSP code project', date: new Date('2022/05/02'
+    )}, {task: 'finish late work', date: new Date('2022/05/19'
+    )}, {task: 'learn more programing', date: new Date('2022/05/01'
     )}
 ]
 
@@ -12,14 +12,13 @@ function fill(){
     var name = document.getElementById('TaskNameinput').value
     var deadline = document.getElementById('TaskDeadlineinput').value
 
+    //Algorithem checks if the inputed date is valid date
     var givendate = JSON.stringify(deadline)
     var currentDate = new Date
-    console.log(currentDate.getDate())
-    console.log(givendate.substring(9,11))
     if(givendate.substring(6,8) >= currentDate.getMonth()+1)
     {
         console.log("passed")
-        if(givendate.substring(9,11) >= currentDate.getDate())
+        if(givendate.substring(9,11) >= currentDate.getDate() || givendate.substring(6,8) > currentDate.getMonth()+1 )
         {
             addTask(name, deadline)
             console.log("Valid Date Adding")
@@ -34,8 +33,6 @@ function fill(){
         alert("invalid date: Given Date has passed")
     }
 
-    //addTask(name , deadline)
-
 }
 
 //function gets called by fill() and adds a new task to the list
@@ -47,6 +44,7 @@ function addTask(TaskName, Deadline)
     console.log("pressed");
     console.log(TaskList);
 
+    //re-sorts the list when new task is added to make sure that when displayed the list is in order by deadline
     var sortedTasks = TaskList.slice().sort((b,a) => b.date - a.date);
  
     //This section for desplaying an array in HTML was made by Anand(stackOverflow)
@@ -61,11 +59,7 @@ function addTask(TaskName, Deadline)
     document.getElementById('display').innerHTML = parsed;
     console.log(parsed);
 }
+
 var sortedTasks = TaskList.slice().sort((a,b) => b.date - a.date);
 
 console.log(sortedTasks);
-
-
-dop = "2022/04/29"
-
-console.log(dop.substring(5,7))
